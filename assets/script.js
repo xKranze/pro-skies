9b032b837a1661ceefa8df288a0f071f7a38ffab
Jimmy Yin <118097335+xKranze@users.noreply.github.com>
2023-02-04 16:47:22 -0800

    added dayjs added search functionality

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 var history = localStorage.getItem("history");
 
-if (history){
+if (history.state){
     history = history.split(",");
     for (i=history.length-1; i>=0; i--){
         var listEl = $('<li>');
@@ -12,11 +12,23 @@ if (history){
 }
 
 function fetchWeather(city) {
-    fetch('https://api.openweathermap.org/data/2.5/forecast?q={'+ city +'}&appid={c7edcd49d6f0c8e1959bb9f207d5c8be}', {
+    fetch('https://api.openweathermap.org/data/2.5/forecast?q='+ city +'&appid=c7edcd49d6f0c8e1959bb9f207d5c8be', {
         cache: 'reload',
         })
         .then((response) => response.json())
         .then(function (data) {
-        console.log(data);
+            if(data){
+                if(data.cod == '404'){
+                    $("#city").text(data.message);
+                }else if(data.cod == "200"){
+                    $("#city").text(data.city.name + " ("+dayjs().format('M/D/YYYY')+") ");
+                }
+            }
         });
 }
+
+$("#search").on('click',function(){
+    if ($("#cityName").val().length>0){
+        fetchWeather($("#cityName").val());
+    }
+});
